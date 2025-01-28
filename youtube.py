007a448eb301680ec46b76ba27445b1dd007a2d6
max vorabhol <max003003003@gmail.com>
2025-01-28 08:47:06 +0700

    init

--- youtube.py
+++ youtube.py
@@ -1,18 +1,37 @@
 import pytchat
 from multiprocessing import Process
 import pandas as pd
+from dotenv import load_dotenv
+from pymongo import MongoClient
+import os
+import json
+
+load_dotenv()
+mongo_user = os.getenv("MONGO_USER")
+mongo_password = os.getenv("MONGO_PASSWORD")
+host = os.getenv("HOST")
 
 
 def load_youtube_chat(video_id):
+    db = MongoClient( host=host, username=mongo_user, password=mongo_password, port=27017)["youtube_chat"]
+    collection = db["chat"]
     yt = pytchat.create(video_id=video_id)
     while yt.is_alive():
         for c in yt.get().sync_items():
             print(f"{c.json()}")
+            document = json.loads( c.json() )
+            document["youtube_id"] = video_id
+            collection.update_one(
+                {"id": document["id"]},  # Match condition (use a unique field, e.g., "id")
+                {"$set": document},  # Fields to set/update
+                upsert=True  # Enable upsert
+            )
 
 def main():
     df = pd.read_csv("yt_list.csv")
     yt_list = df["id"]
     processes = []
+
 
     # Create and start a process for each video ID
     for video_id in yt_list:
